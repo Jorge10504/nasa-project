bebf9f2214965d37936e94170f3c460666a02372
Jorge10504 <georgeunal2016@gmail.com>
2024-05-15 21:51:08 -0500

    Adding a middle function to handle requests for all planets

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,4 @@
-const http = requre("http");
+const http = require("http");
 const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
@@ -7,5 +7,3 @@ const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}...`);
 });
-
-console.log(PORT);
